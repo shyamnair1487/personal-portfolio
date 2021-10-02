567fd3b492b45ec7e059c998f2393d3a65a60c1d
shyam nair <shyamnair1487@gmail.com>
2021-10-02 12:56:23 +0800

    Displayed Blog object on all_blogs page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-	return render(request, 'blog/all_blogs.html')
+	blogs = Blog.objects.all()
+	return render(request, 'blog/all_blogs.html', { 'blogs' : blogs })
 
 	
