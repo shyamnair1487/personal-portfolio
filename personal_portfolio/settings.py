d92b45a77d67c3e81c3432f513f573836c903f05
shyam nair <shyamnair1487@gmail.com>
2021-10-08 12:12:49 +0800

    Local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -126,6 +126,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
 
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production.")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
